549b43036061c381f2fb97f4ff456dd4334e9507
Gemasss <58686523+Gemafauzi@users.noreply.github.com>
2020-05-06 22:00:26 +0700

    upload

--- pratikum/PW_P7_KAMIS08_193040062/Latihan7c/php/login.php
+++ pratikum/PW_P7_KAMIS08_193040062/Latihan7c/php/login.php
@@ -48,11 +48,22 @@ if (isset($_POST['submit'])) {
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <style>
+      .container{
+        width: 300px;
+      }
+      body{
+        margin-top: 200px;
+        
+      }
+        
+    </style>
+
 
     <title>Hello, world!</title>
   </head>
   <body>
-    <div class="countainer">
+    <div class="container">
   <form method="POST">
   <div class="form-group">
     <?php if (isset($eror)):?>
